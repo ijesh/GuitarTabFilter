6034984fb95f1bd4e213679f07e368f569238f18
Ijesh Giri <ijesh@ig-PC.(none)>
2013-03-21 12:16:45 -0600

    Files added

--- ArtistExtracter.py
+++ ArtistExtracter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 from sets import Set
 
@@ -9,7 +11,12 @@ if(len(sys.argv) < 3):
 
 ############# Arguments ##############
 debug = False
+artist = ""
+if len(sys.argv) > 5:
+	artist_file = sys.argv[5]
 inputFile = sys.argv[1]
+#print sys.argv
+#print inputFile
 outputFile = sys.argv[2]
 onlyArtist = False
 inputGenre = False
@@ -37,7 +44,7 @@ def extractSong(song):
 	return song[:index]
 
 def getInput():
-	print "Enter genre for "+ value +":",
+	print "Enter genre for "+ key +":",
 	genre = raw_input()
 	if not genre:
 		return getInput()
@@ -51,18 +58,30 @@ writer = open(outputFile, 'w')
 for line in hdl:
 	if(line.find('http') != -1):
 		words = line.split('/')
+		#print words
 		artist = words[-2]
 		song = extractSong(words[-1])
-		if song not in song_artist_hash:
-			song_artist_hash[song] = artist
+		if artist not in song_artist_hash:
+			song_artist_hash[artist] = song
+
+
+a_hdlr = open(artist_file)
+artists_genre = {}
+for line in a_hdlr:
+	things = line.split(" ")
+	artist = things[0]
+	genre = things[1]
+	if artist not in artists_genre:
+		artists_genre[artist] = genre
 
 for key, value in song_artist_hash.iteritems():
-	if(onlyArtist):
-		if(inputGenre):
-			genre = getInput()
-		writer.write(value+" "+genre+ "\n")
-	else:
-		writer.write(key + "\t" + value+ "\n")
+	if key not in artists_genre:
+		if(onlyArtist):
+			if(inputGenre):
+				genre = getInput()
+			writer.write(key+" "+genre+ "\n")
+		else:
+			writer.write(value + "\t" + value+ "\n")
 
 if(debug):
 	songText = "everlasting_love_crd.htm"
